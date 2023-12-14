c2a7e14f6dfb171d46a3817c88ce58a4f2ad8ec3
JansenSmith <jansen@bstarengineering.com>
2023-12-14 11:44:41 -0500

    retargetted import

--- JanKal_MealVoucher.groovy
+++ JanKal_MealVoucher.groovy
@@ -6,9 +6,9 @@ import javafx.scene.text.Font;
 
 File f = ScriptingEngine
 	.fileFromGit(
-		"https://github.com/JansenSmith/JanKalMealVoucher.git",//git repo URL
+		"https://github.com/JansenSmith/SDXLvoucher.git",//git repo URL
 		"refs/heads/main",//branch
-		"JanKal_MealVoucher.svg"// File from within the Git repo
+		"lightbulb-filament-eureka.svg"// File from within the Git repo
 	)
 println "Extruding SVG "+f.getAbsolutePath()
 SVGLoad s = new SVGLoad(f.toURI())
